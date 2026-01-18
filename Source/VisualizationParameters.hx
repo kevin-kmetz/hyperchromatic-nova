573bb52795a72a03109de590f6c6876801bcd0d8
endofunctor <kevin.kmetz@protonmail.com>
2026-01-17 23:16:03 -0700

    Write script for downloading and running historical love2d partial implementation
    I also updated my Vim file to ignore Vim swap files since I had accidentally committed two of them previously (ugh). I examined them to make sure nothing was leak (though I didn't suspect there would be anyway).
    
    Next up is to write two more scripts to fetch even earlier implementations of hyperchromatic, or programs that preceded it but are influencing it.

--- Source/VisualizationParameters.hx
+++ Source/VisualizationParameters.hx
@@ -4,7 +4,7 @@
  * This will probably have to be renamed at some point, because technically
  * a visualization can be made up of tweening / interpolating between several
  * of these. I'm not quite sure what the "individual units" should be called,
- * thought.
+ * though.
  */
 
 package;
---
+++ obtain_historical_reference.sh
@@ -0,0 +1,28 @@
+# hyperchromatic-nova/obtain_historical_references.sh
+#
+# Obtains one of the earlier implementations of HyperChromatic Nova that
+# was never completed but was functional, written in Lua over Love2d.
+#
+# Love2d will need to be installed to successfully execute this script.
+# Visit "https://love2d.org" for installers and instructions.
+# cURL is also presumed to be present for this script to successfully execute.
+#
+# Run this script from the "hyperchromatic-nova" (project root) directory
+# with the following command, assuming a Linux or Mac environment:
+#   sh obtain_historical_references.sh
+#
+# Instructions (keys):
+#   'c' => change colors
+#   'p' => pause (or unpause if already paused)
+#   'f' => change time direction to forward
+#   'r' => change time direction to reverse (height wrap-around not implemented)
+#   ',' => decrease speed of time
+#   '.' => increase speed of time
+
+mkdir -p historical-references/old-hyperchromatic-nova
+
+curl -o historical-references/old-hyperchromatic-nova/main.lua \
+     https://raw.githubusercontent.com/kevin-kmetz/hyperchromatic-nova/df9990e8c8a2fcad7622749aaa3bfeeb697292dd/main.lua
+
+love historical-references/old-hyperchromatic-nova
+
