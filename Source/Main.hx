8f1df0c85dc0c93242a625f87d1a608062cdec30
Kevin Kmetz <kev.kmetz@gmail.com>
2026-01-19 21:40:00 -0700

    [impl] Get primitive shader displaying
    Just to get something rendering at all before I start to flesh out the actual shader I intend to use.
    
    Next up is to begin implementing actual functionality in the shader that moves towards what I have envisioned.

--- Source/Main.hx
+++ Source/Main.hx
@@ -4,11 +4,48 @@
 
 package;
 
+import openfl.Lib;
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
+import openfl.display.DisplayObjectShader;
 import openfl.display.Sprite;
+import openfl.filters.ShaderFilter;
 
-class Main extends Sprite {
+class CustomShader extends DisplayObjectShader {
+  @:glFragmentBody("
+    gl_FragColor = vec4(
+      openfl_TextureCoordv.y,
+      0.0,
+      openfl_TextureCoordv.x,
+      1.0
+    );
+  ")
+
   public function new() {
     super();
+  }
+}
+
+class Main extends Sprite {
+  private final window = Lib.current.stage.window;
+
+  public function new() {
+    super();
+
+    displayVisuals();
+  }
+
+  private function displayVisuals():Void {
+    final data = new BitmapData(1, 1, false, 0x00FFAA55);
+    final bitmap = new Bitmap(data);
+
+    bitmap.width = window.width;
+    bitmap.height = window.height;
+
+    final custom = new CustomShader();
+    bitmap.filters = [new ShaderFilter(custom)];
+
+    addChild(bitmap);
   }
 }
 
