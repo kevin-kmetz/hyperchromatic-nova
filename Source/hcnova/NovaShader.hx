247a518d4444ceed801df7303a036f55af28ceef
endofunctor <kevin.kmetz@protonmail.com>
2026-01-20 19:34:07 -0700

    [impl] Implement early unanimated fractal brownian motion via GLSL
    Simply ported the custom fbm noise function from my old Love2d program - was simple enough. However, I realized the HTML5 target was failing to render, because it requires loops to be more obviously deterministic (it was claiming the octaves variable was not constant, and worked if I just typed in the number of octaves in the comparison directly).
    
    I'm curious to see if passing in the number of octaves by variable as a uniform will affect whether it runs on web or not.
    
    Next up is to try passing in octaves as a variable to a GLSL uniform and see if it runs on the HTML5 target properly.

--- Source/hcnova/NovaShader.hx
+++ Source/hcnova/NovaShader.hx
@@ -7,12 +7,34 @@ package hcnova;
 import openfl.display.Shader;
 
 final novaFragmentBody = "
-  float noise_val = snoise(openfl_TextureCoordv);
+  int octaves = 6;
+  float persistence = 1.5;
+  float lacunarity = 1.3;
+
+  float totalNoise = 0.0;
+  float currentFrequency = 1.0;
+  float currentAmplitude = 1.0;
+  float summedAmplitudes = 1.0;
+
+  // The following works on desktop targets, but not on the HTML5 target.
+  // On the HTML5 target, it claims that a non-constant can not be part of
+  // the comparison fo the loop. Replacing it with a fixed number works.
+  // Going to see if variables passed in as uniforms rectify this.
+  //
+  for (int i = 0; i < octaves; ++i) {
+    totalNoise += snoise(openfl_TextureCoordv * currentFrequency)
+                  * currentAmplitude;
+    summedAmplitudes += currentAmplitude;
+    currentAmplitude *= persistence;
+    currentFrequency *= lacunarity;
+  }
+
+  float outputNoise = totalNoise / summedAmplitudes;
 
   gl_FragColor = vec4(
-    noise_val,
+    outputNoise,
     0.0,
-    noise_val,
+    outputNoise,
     1.0
   );
 ";
