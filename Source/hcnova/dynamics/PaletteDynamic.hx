a6f9ccae8515808aa97d50df8529c7fc1cadde9c
endofunctor <kevin.kmetz@protonmail.com>
2026-02-02 23:54:15 -0700

    [impl] Write early version of PaletteDynamics class
    Incredibly trivial commit, and am committing pretty much just to get it out of the way for some tweaks I'm thinking of goofing around with.
    
    There were two ideas I've started pondering, which I may need to stop to reflect on. The first is that I need to actually write out what I believe the final flow (or at least a more mature flow) for the program should be, because my approach of just creating what I immediately need or what I think I will need may break down when it comes time to put all the pieces together. I really need to flowchart it, even if it is just on scratch paper.
    
    The next up is regarding the idea that I've been working towards and preparing for, of "tweening" between two separate animations in a perfectly continuous way (the properties morph into the other properties). I'm beginning to wonder if mathematically that would just reduce to a single "normal" visualization as I'm designing them at the moment, and has me questioning if it's even worthwhile to try implementing such a thing. Given that both are periodic, nesting periodic things should reduce again to some periodic thing, which potentially could have been represented as a single "visualization" itself without the tweening. I could be wrong, but I need to think about it some more.
    
    Next up is some rough flowcharting, but as for code, whatever I see as being necessary. I may want to implement a simulated clock class soon, or a HUD class.

---
+++ Source/hcnova/dynamics/PaletteDynamic.hx
@@ -0,0 +1,22 @@
+/*
+ * hyperchromatic-nova/Source/hcnova/dynamics/PaletteDynamic.hx
+ */
+
+package hcnova.dynamics;
+
+import hcnova.Util;
+
+class PaletteDynamic {
+  private static final DELTA_LOWER_BOUND = 0.005;
+  private static final DELTA_UPPER_BOUND = 0.080;
+
+  public final delta:Float;
+
+  public function new() {
+    delta = Util.randomFloat(
+      PaletteDynamic.DELTA_LOWER_BOUND,
+      PaletteDynamic.DELTA_UPPER_BOUND
+    );
+  }
+}
+
